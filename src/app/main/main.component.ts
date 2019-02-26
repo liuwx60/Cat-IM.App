9d3acf47a378f048f6eaed081b4d92e70e22c9c0
liuwx60 <604278879@qq.com>
2019-02-26 17:35:12 +0800

    :lipstick: main page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,7 +3,8 @@ import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-main',
-  templateUrl: './main.component.html'
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
 
